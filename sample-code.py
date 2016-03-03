55abb69ed79089345dc5cd6a8854b92c848a65f1
madhuvijay95 <madhuvijay95@gmail.com>
2016-03-03 04:59:25 -0500

    some feature generation

--- sample-code.py
+++ sample-code.py
@@ -6,6 +6,7 @@
 
 import os
 from collections import Counter
+from datetime import datetime
 try:
     import xml.etree.cElementTree as ET
 except ImportError:
@@ -13,6 +14,7 @@ except ImportError:
 import numpy as np
 from scipy import sparse
 import cPickle as pickle
+import sys
 
 import util
 
@@ -20,6 +22,12 @@ TRAIN_DIR = "train"
 
 call_list = pickle.load(open('tag_list.p', 'r'))
 call_set = set(call_list)
+
+
+#features = ['sleep', 'dump_line']
+features = call_list + ['bytes_sent', 'bytes_received', 'any_sent', 'any_received', 'totaltime']
+
+
 
 #def add_to_set(tree):
 #    for el in tree.iter():
@@ -31,6 +39,7 @@ def create_data_matrix(start_index, end_index, direc="train"):
     classes = []
     ids = [] 
     i = -1
+
     #for datafile in os.listdir(direc):
     #    if datafile == '.DS_Store':
     #        continue
@@ -41,7 +50,9 @@ def create_data_matrix(start_index, end_index, direc="train"):
             continue
 
         i += 1
-        print i
+        if i % 100 == 0:
+            print i
+            sys.stdout.flush()
         if i < start_index:
             continue 
         if i >= end_index:
@@ -67,9 +78,12 @@ def create_data_matrix(start_index, end_index, direc="train"):
         #    #print (elt.tag, elt.attrib, [e[0].tag=='all_section' for e in elt])
         #print
         #print
-        this_row = call_feats(tree)
+
+        this_row_dict = call_feats(tree)
+        this_row = np.array([(this_row_dict[feature] if feature in this_row_dict else 0) for feature in features])
+        #this_row = call_feats(tree)
         if X is None:
-            X = this_row 
+            X = this_row
         else:
             X = np.vstack((X, this_row))
 
@@ -77,24 +91,44 @@ def create_data_matrix(start_index, end_index, direc="train"):
 
 def call_feats(tree):
     #good_calls = ['sleep', 'dump_line']
-    good_calls = call_list
+    #good_calls = call_list + ['bytes_sent', 'bytes_received', 'totaltime']
 
     call_counter = {}
+    call_counter['bytes_sent'] = 0
+    call_counter['bytes_received'] = 0
+    call_counter['any_sent'] = 0
+    call_counter['any_received'] = 0
+    call_counter['totaltime'] = 0
     for el in tree.iter():
         call = el.tag
         if call not in call_counter:
             call_counter[call] = 1
         else:
             call_counter[call] += 1
+        if call == 'send_socket':
+            call_counter['bytes_sent'] += int(el.attrib['buffer_len'])
+            call_counter['any_sent'] = 1
+        elif call == 'recv_socket':
+            call_counter['bytes_received'] += int(el.attrib['buffer_len'])
+            call_counter['any_received'] = 1
+        elif call == 'process':
+            endtime = datetime.strptime(el.attrib['terminationtime'], '%M:%S.%f')
+            starttime = datetime.strptime(el.attrib['starttime'], '%M:%S.%f')
+            call_counter['totaltime'] += (endtime - starttime).total_seconds()
 
-    call_feat_array = np.zeros(len(good_calls))
-    for i in range(len(good_calls)):
-        call = good_calls[i]
-        call_feat_array[i] = 0
-        if call in call_counter:
-            call_feat_array[i] = call_counter[call]
 
-    return call_feat_array
+    ##### TRY ADDING (BINARY?) FEATURES FOR SOCKET NUMBER -- e.g. sockets 1772, 1812, and 1828 are usually Swizzor, while 2068 is never Swizzor
+
+
+    #call_feat_array = np.zeros(len(good_calls))
+    #for i in range(len(good_calls)):
+    #    call = good_calls[i]
+    #    call_feat_array[i] = 0
+    #    if call in call_counter:
+    #        call_feat_array[i] = call_counter[call]
+
+    return call_counter
+    #return call_feat_array
 
 ## Feature extraction
 def main():
@@ -102,13 +136,20 @@ def main():
     X_valid, t_valid, valid_ids = create_data_matrix(10, 15, TRAIN_DIR)
 
     print 'Data matrix (training set):'
-    print X_train.shape
+    print X_train
     print train_ids
     print 'Classes (training set):'
     print t_train
 
     # From here, you can train models (eg by importing sklearn and inputting X_train, t_train).
+
+
     print 'unique tags:', call_set
+    print
+    print
+
+    X, t, ids = create_data_matrix(0, 10000, TRAIN_DIR)
+    pickle.dump((ids, X, t), open('train_data_new.p', 'w'))
 
 if __name__ == "__main__":
     main()
