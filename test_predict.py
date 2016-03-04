2e0cf5f9c9f1a7fee6b4e593f1a456a42063d116
madhuvijay95 <madhuvijay95@gmail.com>
2016-03-03 21:02:27 -0500

    fixed test_predict.py to ensure consistency with other files

--- test_predict.py
+++ test_predict.py
@@ -5,12 +5,8 @@ import csv
 ids, X, t, used_features = pickle.load(open('data_matrix.p', 'r'))
 feature_mask, clf = pickle.load(open('classifier.p', 'r'))
 
-
-#### FIX THE REST
-
-X_test, t_test, ids_test, features = create_data_matrix(100)
-feature_indices = [features.index(feature) for feature in used_features]
-X_test = X_test[:, feature_indices]
+X_test = X[t == -1, feature_mask]
+ids_test = ids[t == -1]
 predictions = clf.predict(X_test)
 predictions = zip(ids_test, predictions)
 
@@ -19,3 +15,17 @@ with open('predictions.csv', 'wb') as output_file:
     writer.writerow(('Id', 'Prediction'))
     for tup in predictions:
         writer.writerow(tup)
+
+#### CAN PROBABLY DELETE THE REST, AS LONG AS EVERYTHING ABOVE THIS WORKS
+
+#X_test, t_test, ids_test, features = create_data_matrix(100)
+#feature_indices = [features.index(feature) for feature in used_features]
+#X_test = X_test[:, feature_indices]
+#predictions = clf.predict(X_test)
+#predictions = zip(ids_test, predictions)
+
+#with open('predictions.csv', 'wb') as output_file:
+#    writer = csv.writer(output_file)
+#    writer.writerow(('Id', 'Prediction'))
+#    for tup in predictions:
+#        writer.writerow(tup)
